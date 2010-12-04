6d55c55b337917189f7a28c66f22fde0a74570ad
egv <g.evstratov@gmail.com>
2010-12-05 00:34:34 +0200

    Added json parsing library

---
+++ Classes/CardManager.h
@@ -0,0 +1,16 @@
+//
+//  CardManager.h
+//  Hebrewet
+//
+//  Created by Gennady Evstratov on 12/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CardManager : NSObject {
+
+}
+
+@end
---
+++ Classes/CardManager.m
@@ -0,0 +1,14 @@
+//
+//  CardManager.m
+//  Hebrewet
+//
+//  Created by Gennady Evstratov on 12/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "CardManager.h"
+
+
+@implementation CardManager
+
+@end
